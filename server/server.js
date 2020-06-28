0e709754ada34bf8ef41004f199ac226dfb90f90
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-27 18:10:29 -0700

    Fix untracked files

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('home page!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
