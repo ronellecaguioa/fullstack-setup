522144e62a8a8cdb0bbb675f24bee87fcea14969
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-27 18:26:18 -0700

    Create webpage

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './stylesheets/App.css'
+
+const App = (props) => {
+  return ( 
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>Welcome to your React App</p>
+    </div>
+   );
+}
+ 
+export default App;
